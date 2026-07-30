c58d2b3fc5d06045a54d26896741ae0ec7d7a9d2
Gangala Tanishka <Tanishka-1004@users.noreply.huggingface.co>
2026-07-30 12:40:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import os
 import gradio as gr
 from google import genai
 
-# # Load API key
-# GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
-# if not GEMINI_API_KEY:
-#     raise RuntimeError("GEMINI_API_KEY not found")
+# Load API key
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+if not GEMINI_API_KEY:
+    raise RuntimeError("GEMINI_API_KEY not found")
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
